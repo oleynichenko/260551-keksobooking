31d65ba833fe6e024e8a468b239fc7da3dbea2cc
Alex Oleynichenko <oleynichenkos@gmail.com>
2018-02-15 23:07:36 +0200

    correcting mistakes #1

--- src/data/entity-data.js
+++ src/data/entity-data.js
@@ -1,4 +1,4 @@
-const Places = [
+const PLACES = [
   `Большая уютная квартира`,
   `Маленькая неуютная квартира`,
   `Огромный прекрасный дворец`,
@@ -9,20 +9,20 @@ const Places = [
   `Неуютное бунгало по колено в воде`
 ];
 
-const Types = [
+const TYPES = [
   `flat`,
   `palace`,
   `house`,
   `bungalo`
 ];
 
-const Time = [
+const TIME = [
   `12:00`,
   `13:00`,
   `14:00`
 ];
 
-const Features = [
+const FEATURES = [
   `wifi`,
   `dishwasher`,
   `parking`,
@@ -31,16 +31,42 @@ const Features = [
   `conditioner`
 ];
 
-const Photos = [
+const PHOTOS = [
   `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
   `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
   `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
 ];
 
+const Price = {
+  MIN: 1000,
+  MAX: 1000000
+};
+
+const Rooms = {
+  MIN: 1,
+  MAX: 5
+};
+
+const Guests = {
+  MIN: 1,
+  MAX: 50
+};
+
+const Location = {
+  X_MIN: 300,
+  X_MAX: 900,
+  Y_MIN: 150,
+  Y_MAX: 500,
+};
+
 module.exports = {
-  Places,
-  Types,
-  Time,
-  Features,
-  Photos
+  PLACES,
+  TYPES,
+  TIME,
+  FEATURES,
+  PHOTOS,
+  Price,
+  Rooms,
+  Guests,
+  Location
 };
--- test/entity-generator.test.js
+++ test/entity-generator.test.js
@@ -1,5 +1,5 @@
 const assert = require(`assert`);
-const {Places, Types, Time, Features, Photos} = require(`../src/data/entity-data`);
+const {PLACES, TYPES, TIME, FEATURES, PHOTOS, Price, Rooms, Guests, Location} = require(`../src/data/entity-data`);
 const {generateEntity} = require(`../src/generator/entity-generator`);
 
 const data = generateEntity();
@@ -7,7 +7,7 @@ const randomPlace = data[Math.floor(Math.random() * data.length)];
 
 describe(`Generate data`, () => {
   it(`should generate data for all Places`, () => {
-    assert.equal(data.length, Places.length);
+    assert.equal(data.length, PLACES.length);
   });
 });
 
@@ -26,54 +26,54 @@ describe(`Random item from generated data should match the data model`, () => {
   describe(`Offer`, () => {
     const offer = randomPlace.offer;
 
-    it(`"title" should be one of Places data`, () => {
-      assert.ok(Places.includes(offer.title));
+    it(`"title" should be one of PLACES data`, () => {
+      assert.ok(PLACES.includes(offer.title));
     });
 
-    it(`"price" should be between 1000 and 1000000`, () => {
-      assert.ok(offer.price >= 1000 && offer.price <= 1000000);
+    it(`"price" should be between ${Price.MIN} and ${Price.MAX}`, () => {
+      assert.ok(offer.price >= Price.MIN && offer.price <= Price.MAX);
     });
 
-    it(`"type" should be one of Types data`, () => {
-      assert.ok(Types.includes(offer.type));
+    it(`"type" should be one of TYPES data`, () => {
+      assert.ok(TYPES.includes(offer.type));
     });
 
-    it(`"rooms" should be between 1 and 5`, () => {
-      assert.ok(offer.rooms >= 1 && offer.rooms <= 5);
+    it(`"rooms" should be between ${Rooms.MIN} and ${Rooms.MAX}`, () => {
+      assert.ok(offer.rooms >= Rooms.MIN && offer.rooms <= Rooms.MAX);
     });
 
-    it(`"guests" should be between 1 and 50`, () => {
-      assert.ok(offer.guests >= 1 && offer.guests <= 50);
+    it(`"guests" should be between ${Guests.MIN} and ${Guests.MAX}`, () => {
+      assert.ok(offer.guests >= Guests.MIN && offer.guests <= Guests.MAX);
     });
 
-    it(`"checkin" should be one of Time data`, () => {
-      assert.ok(Time.includes(offer.checkin));
+    it(`"checkin" should be one of TIME data`, () => {
+      assert.ok(TIME.includes(offer.checkin));
     });
 
-    it(`"checkout" should be one of Time data`, () => {
-      assert.ok(Time.includes(offer.checkout));
+    it(`"checkout" should be one of TIME data`, () => {
+      assert.ok(TIME.includes(offer.checkout));
     });
 
-    it(`"features" first item should be one of Features data or feature's length shoul be 0`, () => {
-      assert.ok(Features.includes(offer.features[0] || offer.features.length === 0));
+    it(`"features" first item should be one of FEATURES data or feature's length should be 0`, () => {
+      assert.ok(offer.features.length === 0 || FEATURES.includes(offer.features[0]));
     });
 
     it(`"description" is empty string`, () => {
       assert.ok(offer.description === ``);
     });
 
-    it(`first item of "photos" should be one of Photos data`, () => {
-      assert.ok(Photos.includes(offer.photos[0]));
+    it(`first item of "photos" should be one of PHOTOS data`, () => {
+      assert.ok(PHOTOS.includes(offer.photos[0]));
     });
   });
 
   describe(`Location`, () => {
-    it(`"location x" should be between 300 and 900`, () => {
-      assert.ok(randomPlace.location.x >= 300 && randomPlace.location.x <= 900);
+    it(`"location x" should be between ${Location.X_MIN} and ${Location.X_MAX}`, () => {
+      assert.ok(randomPlace.location.x >= Location.X_MIN && randomPlace.location.x <= Location.X_MAX);
     });
 
-    it(`"location y" should be between 150 and 500`, () => {
-      assert.ok(randomPlace.location.y >= 150 && randomPlace.location.y <= 500);
+    it(`"location y" should be between ${Location.Y_MIN} and ${Location.Y_MAX}`, () => {
+      assert.ok(randomPlace.location.y >= Location.Y_MIN && randomPlace.location.y <= Location.Y_MAX);
     });
   });
 });
